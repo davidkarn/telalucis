2774458088d858d043bf6623767ceac54bc6fb9c
David Karn <david@webdever.net>
2026-04-12 16:56:39 -0500

    split paragraphs, skip hyphens when parsing text

--- src/pdf_parser/stbernardino.clj
+++ src/pdf_parser/stbernardino.clj
@@ -17,13 +17,13 @@
 
 (defn blocks-in-hocr
   [contents]
-  (sel/select (sel/class "ocr_carea") testcontents))
+  (sel/select (sel/class "ocr_carea") contents))
 
 (defn get-block-dimensions
   [block]
   (let [positions (map #(Integer/parseInt %)
-                       (rest (str/split (:title (:attrs block))
-                                        #" ")))]
+                       (take 4 (rest (str/split (:title (:attrs block))
+                                                #";? "))))]
     {:left (nth positions 0)
      :top (nth positions 1)
      :right (nth positions 2)
@@ -35,11 +35,15 @@
   [block]
   (count (sel/select (sel/class "ocr_line") block)))
 
+(defn block-lines
+  [block]
+  (sel/select (sel/class "ocr_line") block))
+
+
 (defn read-single-hocr-block-text
   [block]
   (str/replace
    (->> block
-        
         (sel/select (sel/tag "span"))
         (map :content)
         (flatten)
@@ -48,4 +52,41 @@
         (str/join " "))
    "- " "-"))
 
+(defn is-line-indented
+  [block-dimensions line] 
+  (let [line-dimensions (get-block-dimensions line)
+        line-char-width (/ (:width line-dimensions)
+                           (count (read-single-hocr-block-text line)))]
+    (>
+     (- (:left line-dimensions) (:left block-dimensions))
+     (/ line-char-width 2))))
 
+(defn line-text
+  [line]
+  (let [text (read-single-hocr-block-text line)]
+    (if (= (last text) \-)
+      (subs line 0 (- (count line) 1))
+      (str text " "))))
+
+(defn paragraphs-in-hocr-block
+  [block]
+  (loop [lines (block-lines block)
+         paragraphs []
+         this-paragraph ""
+         dimensions (get-block-dimensions block)]
+    (cond
+      (= (count lines) 0)
+      (concat paragraphs [this-paragraph])
+
+      (is-line-indented dimensions (first lines))
+      (recur (rest lines)
+             (concat paragraphs [this-paragraph])
+             (line-text (first lines))
+             dimensions)
+
+      :else
+      (recur (rest lines)
+             paragraphs
+             (str this-paragraph (line-text (first lines)))
+             dimensions))))
+    
