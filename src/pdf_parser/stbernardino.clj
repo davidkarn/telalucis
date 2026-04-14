5e9cc78fd26da35244a69fb3144f3f3544638513
David Karn <david@webdever.net>
2026-04-13 20:44:00 -0500

    fix issues with parsing and combining pages

--- src/pdf_parser/stbernardino.clj
+++ src/pdf_parser/stbernardino.clj
@@ -3,6 +3,7 @@
   (:require [clojure.string :as str])
   (:require [hickory.core :as hickory])
   (:require [hickory.select :as sel])
+  (:require [clojure.data.json :as json])
   (:require [telalucis.core :as core]))
 
 (import '[org.jsoup Jsoup])
@@ -57,13 +58,16 @@
    "- " "-"))
 
 (defn is-line-indented
-  [block-dimensions line]
+  [block-dimensions line prev-line]
   (let [line-dimensions (get-block-dimensions line)
         line-char-width (/ (:width line-dimensions)
                            (count (read-single-hocr-block-text line)))]
     (>
-     (- (:left line-dimensions) (:left block-dimensions))
-     (/ line-char-width 2))))
+     (- (:left line-dimensions)
+        (if prev-line
+          (:left (get-block-dimensions prev-line))
+          (:left block-dimensions)))
+     (* 2 line-char-width))))
 
 (defn line-text
   [line]
@@ -75,6 +79,7 @@
 (defn paragraphs-in-hocr-block
   [block]
   (loop [lines          (block-lines block)
+         prev-line      nil
          paragraphs     []
          this-paragraph ""
          dimensions     (get-block-dimensions block)]
@@ -82,20 +87,22 @@
       (= (count lines) 0)
       (concat paragraphs [this-paragraph])
 
-      (is-line-indented dimensions (first lines))
+      (is-line-indented dimensions (first lines) prev-line)
       (recur (rest lines)
+             (first lines)
              (concat paragraphs [this-paragraph])
              (line-text (first lines))
              dimensions)
 
       :else
       (recur (rest lines)
+             (first lines)
              paragraphs
              (str this-paragraph (line-text (first lines)))
              dimensions))))
 
 (defn add-notes-to-paragraphs
-  [main-paragraphs note-blocks main-paragraph-breaks top bottom]
+  [main-paragraphs note-blocks main-paragraph-breaks top bottom page-number]
   (let [line-height      (/ (- bottom top)
                             (max 1 (count main-paragraph-breaks)))
         para-line-breaks (map second
@@ -114,21 +121,22 @@
     (drop-last
      1
      (map (fn [pos i]
-            {:index      i
-             :text       (nth main-paragraphs i 0)
-             :side-notes (map
-                          (fn [b] (read-single-hocr-block-text (:block b)))
-                          (filter (fn [nb] (and (> (:top (:dimensions nb))
-                                                   (second pos))
-                                                (< (:top (:dimensions nb))
-                                                   (second (nth breaks-with-pos (+ i 1))))))
-                                  note-blocks))})
+            {:index       i
+             :page-number page-number
+             :text        (nth main-paragraphs i 0)
+             :side-notes  (map
+                           (fn [b] (read-single-hocr-block-text (:block b)))
+                           (filter (fn [nb] (and (> (:top (:dimensions nb))
+                                                    (second pos))
+                                                 (< (:top (:dimensions nb))
+                                                    (second (nth breaks-with-pos (+ i 1))))))
+                                   note-blocks))})
           breaks-with-pos
           (range 0 (count breaks-with-pos))))))
      
 
 (defn parse-page
-  [page]
+  [page page-number]
   (let [blocks                (blocks-in-hocr page)
         block-sizes           (map (fn [block]
                                      (let [dimensions (get-block-dimensions block)]
@@ -158,15 +166,23 @@
                                                   (:bottom (:dimensions main-block)))))
                                       block-sizes)
         main-paragraphs       (paragraphs-in-hocr-block (:block main-block))
-        main-paragraph-breaks (map #(is-line-indented (:dimensions main-block) %)
-                                   (block-lines (:block main-block)))]
+        main-lines            (block-lines (:block main-block))
+        main-paragraph-breaks (map-indexed (fn [i line]
+                                             (is-line-indented (:dimensions main-block)
+                                                               line
+                                                               (if (> i 0)
+                                                                 (nth main-lines (- i 1))
+                                                                 nil)))
+                                           main-lines)]
 
     {:footnotes        (map #(read-single-hocr-block-text (:block %)) footnotes)
+     :page-number      page-number
      :text             (add-notes-to-paragraphs main-paragraphs
                                                 (concat left-blocks right-blocks)
                                                 main-paragraph-breaks
                                                 (:top (:dimensions main-block))
-                                                (:bottom (:dimensions main-block)))
+                                                (:bottom (:dimensions main-block))
+                                                page-number)
      :starts-with-para (first main-paragraph-breaks)}))
 
 (defn combine-paragraphs
@@ -174,29 +190,35 @@
   (concat [{:footnotes  (concat (or (:footnotes last-para) [])
                                 (:footnotes next-page-text))
             :side-notes (concat (:side-notes last-para)
-                                (:side-notes (first next-page-text)))
+                                (:side-notes (first (:text next-page-text))))
             :text       (str (:text last-para)
-                             (:text (:first next-page-text)))}]
+                             (:text (first (:text next-page-text))))}]
           (rest (:text next-page-text))))
                                 
 (defn parse-stbernardino-book
-  []
-  (let [pages (range 8 256)]
-    (loop [page       8
-           paragraphs []]
-      (clojure.pprint/pprint page)
-      (if (>= page (last pages))
-        paragraphs
-        (if (#{115 167} page)
-          (recur (+ page 1) paragraphs)
-          (let [page-text (parse-page (pull-page page))]
-            (if (or (= (count paragraphs) 0)
-                    (:starts-with-para page-text))
-              (recur (+ page 1)
-                     (concat (drop-last paragraphs)
-                             (combine-paragraphs (last paragraphs) page-text)))
-              (recur (+ page 1)
-                     (concat paragraphs (concat [(assoc (first (:text page-text))
-                                                        :footnotes
-                                                        (:footnotes page-text))]
-                                                (rest (:text page-text))))))))))))
+  ([] (parse-stbernardino-book 8 256))
+  ([start-page end-page]
+   (let [pages (range start-page end-page)]
+     (loop [page       start-page
+            paragraphs []]
+       (clojure.pprint/pprint page)
+       (if (>= page (last pages))
+         paragraphs
+         (if (#{115 167} page)
+           (recur (+ page 1) paragraphs)
+           (let [page-text (parse-page (pull-page page) page)]
+             (if (or (= (count paragraphs) 0)
+                     (:starts-with-para page-text))
+               (recur (+ page 1)
+                      (concat paragraphs (concat [(assoc (first (:text page-text))
+                                                         :footnotes
+                                                         (:footnotes page-text))]
+                                                 (rest (:text page-text)))))
+               (recur (+ page 1)
+                      (concat (drop-last paragraphs)
+                              (combine-paragraphs (last paragraphs) page-text)))))))))))
+
+(defn write-book
+  [path]
+  (with-open [wrtr (io/writer path)]
+    (.write wrtr (json/write-str (parse-stbernardino-book)))))
